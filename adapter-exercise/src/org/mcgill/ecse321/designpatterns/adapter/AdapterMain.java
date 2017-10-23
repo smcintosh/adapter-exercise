534586d92b9bb29cdee50489f98292b3429723b1
Shane McIntosh <shanemcintosh@acm.org>
2017-10-22 21:03:45 -0400

    Initial import of Adapter example

---
+++ adapter-exercise/src/org/mcgill/ecse321/designpatterns/adapter/AdapterMain.java
@@ -0,0 +1,26 @@
+package org.mcgill.ecse321.designpatterns.adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdapterMain {
+	public static void main(String[] args) {
+		// Old objects
+		Line line = new Line();
+		Rectangle rectangle = new Rectangle();
+		
+		// Adapt them with adapters
+		List<Shape> shapes = new ArrayList<Shape>();
+		shapes.add(new LineAdapter(line));
+		shapes.add(new RectangleAdapter(rectangle));
+		
+		// Code can now operate using common interface
+		int x1 = 7;
+		int y1 = 23;
+		int x2 = 37;
+		int y2 = 70;
+		for (Shape shape : shapes) {
+			shape.draw(x1, y1, x2, y2);
+		}
+	}
+}
---
+++ adapter-exercise/src/org/mcgill/ecse321/designpatterns/adapter/Rectangle.java
@@ -0,0 +1,8 @@
+package org.mcgill.ecse321.designpatterns.adapter;
+
+public class Rectangle {
+	public void draw(int x, int y, int width, int height) {
+		System.out.println("Rectangle with coordinate left-down point (" + x
+				+ ";" + y + "), width: " + width + ", height: " + height);
+	}
+}

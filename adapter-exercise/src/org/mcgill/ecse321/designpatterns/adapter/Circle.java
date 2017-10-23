a1be5320bc0cfd302957229b94a5cc64684b0479
Shane McIntosh <shanemcintosh@acm.org>
2017-10-22 21:12:31 -0400

    Add Circle class for exercise

---
+++ adapter-exercise/src/org/mcgill/ecse321/designpatterns/adapter/Circle.java
@@ -0,0 +1,8 @@
+package org.mcgill.ecse321.designpatterns.adapter;
+
+public class Circle {
+	public void draw(int x, int y, int radius) {
+		System.out.println("Circle with centre at (" + x
+				+ ";" + y + "), with a radius of " + radius);
+	}
+}

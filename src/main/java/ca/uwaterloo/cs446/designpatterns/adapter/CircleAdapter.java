74a7a8b23af3b3b0774eeece9c93f241ca634060
Khalid Talakshi <khalid.talakshi@outlook.com>
2022-02-28 09:20:34 -0500

    added documentation

--- src/main/java/ca/uwaterloo/cs446/designpatterns/adapter/CircleAdapter.java
+++ src/main/java/ca/uwaterloo/cs446/designpatterns/adapter/CircleAdapter.java
@@ -9,8 +9,12 @@ public class CircleAdapter implements Shape {
 
    @Override
    public void draw(int x1, int y1, int x2, int y2) {
+       // Center is the difference between the two points
        int centerX = (Math.max(x1, x2) - Math.min(x1, x2)) / 2;
        int centerY = (Math.max(y1, y2) - Math.min(y1, y2)) / 2;
-       adaptee.draw(centerX, centerY, Math.min(Math.abs(x1 - x2), Math.abs(y1 - y2)));
+
+       // radius is the minimum distance between x and y
+       int radius = Math.min(Math.abs(x1 - x2), Math.abs(y1 - y2));
+       adaptee.draw(centerX, centerY, radius);
    }
 }
